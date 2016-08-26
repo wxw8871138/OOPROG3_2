dc0074492ab37953882f4e7436352f8aec3615e1
WangXinwei <WangXinwei@DESKTOP-6VHU3KN>
2016-08-26 13:52:57 +0800

    Add project files.

---
+++ OOPROG3_2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPROG3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer y = new Customer("Tan Ah Kow", "20, Seaside Road", "XXX20", new DateTime(1989, 10, 11));
+            Customer z = new Customer("Kim Lee Keng", "2, Rich View", "XXX9F", new DateTime(1993, 4, 25));
+            BankAccout2 a = new BankAccout2("001-001-001", y, 2000);
+            BankAccout2 b = new BankAccout2("001-001-002", z, 5000);
+            
+            Console.WriteLine(a.Show());
+            Console.WriteLine(b.Show());
+            a.Deposit(100);
+            Console.WriteLine(a.Show());
+            a.Withdraw(200);
+            Console.WriteLine(a.Show());
+            a.TransferTo(300, b);
+            Console.WriteLine(a.Show());
+            Console.WriteLine(b.Show());
+
+        }
+    }
+    class Customer
+    {
+        private string name;
+        private string address;
+        private string passportNo;
+        private DateTime dateOfBirth;
+
+        public Customer(string name, string address, string passportNo, DateTime dateOfBirth)
+        {
+            this.name = name;
+            this.address = address;
+            this.passportNo = passportNo;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+        public int GetAge()
+        {
+            int age = 0;
+
+            return age;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+
+    }
+
+    class BankAccout2
+    {
+        private string accountNo;
+        private Customer acountHolder;
+        private double balance;
+
+        public BankAccout2(string accountNo, Customer acountHolder, double balance)
+        {
+            this.accountNo = accountNo;
+            this.acountHolder = acountHolder;
+            this.balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance = balance + amount;
+
+        }
+
+        public void Deposit(double amount)
+        {
+            balance = balance - amount;
+
+        }
+
+        public void TransferTo(double amount, BankAccout2 another)
+        {
+            balance = balance - amount;
+            another.Withdraw(amount);
+        }
+
+        public string Show()
+        {
+            string info;
+            info = acountHolder.GetName() + ":" + Convert.ToString(balance);
+            return info;
+        }
+    }
+
+
+}
